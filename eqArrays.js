498a5922d0c828cb0197304d19286f1c93f83aa2
spiritxhx <spiritxhx@gmail.com>
2019-06-25 13:49:47 -0400

    debug the eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const eqArrays = (array1, array2)=>{
+  if (array1.length!==array2.length){
+    return false;
+  } else {
+    for (let i=0;i<array1.length;i++){
+      if (array1[i]!==array2[i])
+      return false;
+    }
+    return true;
+  }
+}
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', "3"]), true);
