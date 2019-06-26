effc7a79238fe99cf47deba580f2f17be933c515
spiritxhx <spiritxhx@gmail.com>
2019-06-25 20:23:03 -0400

    add the countLetter.js

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const countLetters = word => {
+  let result = {};
+  for (let i = 0; i < word.length; i++) {
+    if (result[word[i]]) {
+      result[word[i]]++;
+    } else {
+      result[word[i]] = 1;
+    }
+  }
+  return result;
+}
+
+assertEqual(countLetters('hello')['l'], 2);
