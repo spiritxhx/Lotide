0aa56c3d5042b0953298a831c2bc9992bbbc1349
spiritxhx <spiritxhx@gmail.com>
2019-06-25 20:33:26 -0400

    add the letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = (actual, expected) => {
+  if (actual.length || expected.length) {
+    if (eqArrays(actual, expected) === true) {
+      return (`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return (`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      return (`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return (`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i])
+        return false;
+    }
+    return true;
+  }
+}
+
+
+const letterPositions = word => {
+  let result = {};
+  for (let i = 0; i < word.length; i++) {
+    if (result[word[i]]) {
+      result[word[i]].push(i);
+    } else {
+      result[word[i]]=[i];
+    }
+  }
+  return result;
+}
+console.log(assertArraysEqual(letterPositions('hello').h, [0]));
