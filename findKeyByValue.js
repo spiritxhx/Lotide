36b3f6b9bc05264b93bf6240487efa24dc5c0b51
spiritxhx <spiritxhx@gmail.com>
2019-06-26 15:51:56 -0400

    simplify the findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,9 +6,7 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-const findKeyByValue = (obj, value) => {
-  return Object.keys(obj).find(key => obj[key] === value);
-}
+const findKeyByValue = (obj, value) => Object.keys(obj).find(key => obj[key] === value);
 
 
 const bestTVShowsByGenre = {
