1cf34b4a9313adee84b37933b854e85c97c23f0f
spiritxhx <spiritxhx@gmail.com>
2019-07-01 16:00:07 -0400

    small adjustments in assertArraysEqual to make the function console log the result

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,9 +3,9 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = (actual, expected) => {
 
   if (eqArrays(actual, expected) === true) {
-    return (`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
+    console.log (`\u{1f600} \u{1f600}  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return (`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
+    console.log (`\u{1f602} \u{1f602}  Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
--- assertObectsEqual.js
+++ assertObectsEqual.js
@@ -2,9 +2,9 @@ const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = (actual, expected) => {
   if (eqObjects(actual, expected)) {
-    return(`\u{1f600} \u{1f600}  Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+    console.log(`\u{1f600} \u{1f600}  Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
   } else {
-    return(`\u{1f602} \u{1f602}  Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+    console.log(`\u{1f602} \u{1f602}  Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
   }
 }
 
